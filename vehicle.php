3a9c86e80c92ac838addc60b2eccef9de2ea90ee
ivancp54 <icozar21@jaumebalmes.net>
2022-09-29 19:17:00 +0200

    parte ivan

---
+++ vehicle.php
@@ -0,0 +1,28 @@
+<?php
+
+class Vehicle{
+    private $ruedas
+    private $marca
+    private $modelo
+    private $puertas
+    private $motor
+
+    function __construct(){}
+
+    function __construct($marca){
+        $this->$marca = $marca
+    }
+
+    function __construct($marca, $ruedas, $modelo, $puertas, $motor){
+        $this->$ruedas = $ruedas
+        $this->$modelo = $modelo
+        $this->$marca = $marca
+        $this->$puertas = $puertas
+        $this->$motor = $motor
+
+    }
+
+}
+
+
+?>
