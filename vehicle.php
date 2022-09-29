bcafa9a4dec7cfc4e5828c84019c596a370fcf71
andrescarles <acarles@jaumebalmes.net>
2022-09-29 20:15:24 +0200

    parte andres

--- vehicle.php
+++ vehicle.php
@@ -7,20 +7,115 @@ class Vehicle{
     private $puertas;
     private $motor;
 
-    function __construct(){}
-
-    function __construct($marca){
-        $this->$marca = $marca;
+    /**
+     * @param $ruedas
+     * @param $marca
+     * @param $modelo
+     * @param $puertas
+     * @param $motor
+     */
+    public function __construct($ruedas, $marca, $modelo, $puertas, $motor)
+    {
+        $this->ruedas = $ruedas;
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->puertas = $puertas;
+        $this->motor = $motor;
     }
 
-    function __construct($marca, $ruedas, $modelo, $puertas, $motor){
-        $this->$ruedas = $ruedas;
-        $this->$modelo = $modelo;
-        $this->$marca = $marca;
-        $this->$puertas = $puertas;
-        $this->$motor = $motor;
 
+
+    /**
+     * @return mixed
+     */
+    public function getRuedas()
+    {
+        return $this->ruedas;
     }
+
+    /**
+     * @param mixed $ruedas
+     */
+    public function setRuedas($ruedas)
+    {
+        $this->ruedas = $ruedas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * @param mixed $marca
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * @param mixed $modelo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPuertas()
+    {
+        return $this->puertas;
+    }
+
+    /**
+     * @param mixed $puertas
+     */
+    public function setPuertas($puertas)
+    {
+        $this->puertas = $puertas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMotor()
+    {
+        return $this->motor;
+    }
+
+    /**
+     * @param mixed $motor
+     */
+    public function setMotor($motor)
+    {
+        $this->motor = $motor;
+    }
+
+    public function kmRecorreguts()
+    {
+        return rand(0,100);
+    }
+    public function vehicleCreat()
+    {
+        return $this->modelo;
+        return $this->motor;
+    }
+
+
     
     public function __toString()
     {
@@ -28,6 +123,4 @@ class Vehicle{
     }
 
 }
-
-
 ?>
