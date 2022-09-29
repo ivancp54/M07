54fab0efa388166408e7084fea430c43e41d2b87
ivancp54 <icozar21@jaumebalmes.net>
2022-09-29 20:03:22 +0200

    parte ivan actualizado

--- vehicle.php
+++ vehicle.php
@@ -1,25 +1,30 @@
 <?php
 
 class Vehicle{
-    private $ruedas
-    private $marca
-    private $modelo
-    private $puertas
-    private $motor
+    private $ruedas;
+    private $marca;
+    private $modelo;
+    private $puertas;
+    private $motor;
 
     function __construct(){}
 
     function __construct($marca){
-        $this->$marca = $marca
+        $this->$marca = $marca;
     }
 
     function __construct($marca, $ruedas, $modelo, $puertas, $motor){
-        $this->$ruedas = $ruedas
-        $this->$modelo = $modelo
-        $this->$marca = $marca
-        $this->$puertas = $puertas
-        $this->$motor = $motor
+        $this->$ruedas = $ruedas;
+        $this->$modelo = $modelo;
+        $this->$marca = $marca;
+        $this->$puertas = $puertas;
+        $this->$motor = $motor;
 
+    }
+    
+    public function __toString()
+    {
+        return self::class . ": " . $this->marca . " " . $this->modelo . " " . $this->motor . " " . $this->puertas . " " . $this->ruedas . " " ;
     }
 
 }
