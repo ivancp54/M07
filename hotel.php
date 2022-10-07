8d6346ec706435f4ae21fd695cfcb22b1a9388de
ivancp54 <113448066+ivancp54@users.noreply.github.com>
2022-10-07 17:20:33 +0200

    Add files via upload
    hotel/restaurante

---
+++ hotel.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            var x;
+            $numero="0","1","2","3","4","5","6","7","8","9";
+            $comensales=
+        ?>
+        <p> La mesa <?php echo $numero[0]?> tiene </p>
+    </body>
+</html>
